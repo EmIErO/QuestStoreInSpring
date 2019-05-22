e627991073f904aea5ae1d3cd27073aad5d2bc48
EmIErO <mira.bryja@gmail.com>
2019-05-22 09:53:00 +0200

    Add model of Artifact.

---
+++ src/main/java/com/codecool/model/Artifact.java
@@ -0,0 +1,68 @@
+package com.codecool.model;
+
+import java.util.Objects;
+
+public class Artifact {
+
+    private int artifactId;
+    private String name;
+    private int price;
+    private String description;
+    private String category;
+
+    public Artifact(String name, int price, String description, String category) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+    }
+
+    public int getArtifactId() {
+        return artifactId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Artifact artifact = (Artifact) o;
+        return artifactId == artifact.artifactId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(artifactId);
+    }
+}
